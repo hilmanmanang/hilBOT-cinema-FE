bcb17ce54eeba85230e5e889120c647ed0aecab0
Hilman Ahmad <hilmanmanang@gmail.com>
2020-04-13 22:38:54 +0800

    adding component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainPageComponent } from './user/main-page/main-page.component';
+import { MovieComponent } from './user/movie/movie.component';
+import { MyTicketComponent } from './user/my-ticket/my-ticket.component';
+import { LoginComponent } from './login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainPageComponent,
+    children: [
+      {
+        path: '',
+        component: MovieComponent
+      },
+      {
+        path: 'my-tiket',
+        component: MyTicketComponent
+      }
+    ]
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+
+import { MainPageComponent } from './user/main-page/main-page.component';
+import { MyTicketComponent } from './user/my-ticket/my-ticket.component';
+import { LoginComponent } from './login/login.component';
+import { MovieComponent } from './user/movie/movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    MyTicketComponent,
+    LoginComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
